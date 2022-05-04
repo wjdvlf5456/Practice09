fd05580af99fe2cdf4d86d71216438565f56ce2e
최정필 <wjdvlf1407@gmail.com>
2022-05-04 17:38:17 +0900

    Ex03 상품정보 입력

--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -1,10 +1,44 @@
 package com.javaex.ex03;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GoodsApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-       
-    }
+		Scanner sc = new Scanner(System.in);
+
+		List<Goods> gList = new ArrayList<Goods>();
+
+		System.out.println("상품을 입력해주세요 (종료: q)");
+
+		while (true) {
+
+			String str = sc.nextLine();
+
+			if (str.equals("q")) {
+				System.out.println("[입력완료]");
+				System.out.println("==============================================");
+				break;
+			}
+			String[] info = str.split(",");
+
+			Goods g = new Goods(info[0], Integer.parseInt(info[1]), Integer.parseInt(info[2]));
+			gList.add(g);
+
+		}
+		int sum = 0;
+
+		for (Goods i : gList) {
+			i.showInfo();
+			sum += i.getCount();
+		}
+		System.out.println("모든 상품의 갯수는 " + sum + "개입니다.");
+
+		sc.close();
+
+	}
 
 }
